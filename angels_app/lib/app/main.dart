828986a31aaf9ce1f1478fed131713d1d0e488a0
Thiago Barros <bunbismuth@proton.me>
2024-07-05 09:00:43 -0300

    feat: added device_preview

--- angels_app/lib/app/main.dart
+++ angels_app/lib/app/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(DevicePreview(enabled: true, builder: (context) => const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
