2a10d9244078b4dd3524d0e1b3683cc268ef0681
Guiioff <guilhermerivaldy288@hotmail.com>
2024-07-16 11:05:21 -0300

    feat: alterando home

--- angels_app/lib/main.dart
+++ angels_app/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: "Angels App",
-      home: GestantesPage(),
+      home: HomeScreen(),
     );
   }
 }
